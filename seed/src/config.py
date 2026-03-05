010662f49737081a8461a5378e11ae7053f3ee71
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-03-05 14:57:03 +0100

    add load_config function to return EtlConfig from environment variables

--- seed/src/config.py
+++ seed/src/config.py
@@ -1,9 +1,20 @@
 import os
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from dotenv import load_dotenv
 
+# Define immutable EtlConfig dataclass
 @dataclass(frozen=True)
 class EtlConfig:
     csv_path: str
     chunk_size: int
     sql_uri: str
+
+# Define function to return EtlConfig object with .env variables
+def load_config() -> EtlConfig:
+    load_dotenv() # From .env file to os.environ
+
+    return EtlConfig(
+        csv_path=os.environ.get("CSV_PATH"),
+        chunk_size=int(os.environ.get("CHUNK_SIZE")),
+        sql_uri=os.environ.get("SQL_URI"),
+    )
--- seed/src/main.py
+++ seed/src/main.py
@@ -1,6 +1,14 @@
 import pandas as pd
+
 df = pd.read_csv("./data/BooksDatasetClean.csv", nrows=5)
 print(list(df.columns))
 print(df.head(5))
 
 print(df.info())
+
+from config import load_config
+
+config = load_config()
+print("csv_path:", config.csv_path)
+print("chunk_size:", config.chunk_size)
+print("sql_uri:", config.sql_uri)
