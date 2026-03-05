be962e5500f37fab3eecb77e0645ea15c00fe525
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-03-05 14:32:21 +0100

    create EtlConfig dataclass to store .env-variables. Make it immutable

--- seed/src/config.py
+++ seed/src/config.py
@@ -1,3 +1,9 @@
 import os
 from dataclasses import dataclass, field
 from dotenv import load_dotenv
+
+@dataclass(frozen=True)
+class EtlConfig:
+    csv_path: str
+    chunk_size: int
+    sql_uri: str
