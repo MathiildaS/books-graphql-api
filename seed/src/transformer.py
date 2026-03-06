f59036ab54280ed500dd28f436574cffef2fbd18
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-03-06 15:31:26 +0100

    Define function to return split authors without "and"

--- seed/src/transformer.py
+++ seed/src/transformer.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Import the regular expression library to clean data from (COM), (EDT) etc.
 import re
 
-# Define a function to covert NaNs and remove whitespaces
+# Define a function to covert NaNs, remove whitespaces, parentheses and "By" from authors names
 def text_clean(rawDataValue):
     if pd.isna(rawDataValue):
         return None # Convert NaN to None to tell PostgreSQL to store NULL
@@ -14,3 +14,15 @@ def text_clean(rawDataValue):
     rawDataValue = re.sub(r'^by\s+', '', rawDataValue, flags=re.IGNORECASE) # Remove "By" before authors names
 
     return rawDataValue.strip() # Remove whitespaces at the beginning and end of string
+
+def extract_authors(rawAuthorsValue):
+    rawAuthorsValue = text_clean(rawAuthorsValue) # Clean the raw authors value
+    if not rawAuthorsValue:
+        return [] # Return empty list if no value (None, NaN, empty string)
+    
+    authors = []
+    
+    for author in rawAuthorsValue.split(" and "): # Split authors by "and"
+        author_clean = author.strip() # Remove whitespaces before and after
+        authors.append(author_clean) # Add "cleaned" author to list of authors
+    return authors
