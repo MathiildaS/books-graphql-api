58373177ec5381b28da8ea703954d6edddb09dc1
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-03-06 14:55:53 +0100

    Add and define a function to convert data values from NaN to None and clean data from text within and parentheses

--- seed/src/transformer.py
+++ seed/src/transformer.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 # Import the regular expression library to clean data from (COM), (EDT) etc.
 import re
+
+# Define a function to covert NaNs and remove whitespaces
+def text_clean(rawDataValue):
+    if pd.isna(rawDataValue):
+        return None # Convert NaN to None to tell PostgreSQL to store NULL
+    
+    rawDataValue = str(rawDataValue) # Convert to string
+    rawDataValue = re.sub(r'\s*\(.*?\)\s*', '', rawDataValue) # Remove text within and parentheses and whitespaces
+    return rawDataValue.strip() # Remove whitespaces at the beginning and end of string
